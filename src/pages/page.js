af7392f45b88cba7c07a8281165f4db5df34f00c
Generosa Litton <glcodeworks@gmail.com>
2022-10-04 14:21:55 -0500

    fixed page link

--- src/pages/page.js
+++ src/pages/page.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Link } from "gatsby";
 
 const Page = () => {
-  return <div>This is a page</div>
-}
+  return (
+    <div>
+      This is a new page now go back <Link to="/">home</Link>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
